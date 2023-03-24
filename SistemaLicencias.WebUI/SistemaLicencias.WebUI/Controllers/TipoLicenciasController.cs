81544b8e055b16d1a537ff5e5e2c868a6203c386
SRMAZ2023 <phynomo09099@gmail.com>
2023-03-24 11:07:40 -0600

    Ahora si, cagadero con Json

---
+++ SistemaLicencias.WebUI/SistemaLicencias.WebUI/Controllers/TipoLicenciasController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SistemaLicencias.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SistemaLicencias.WebUI.Controllers
+{
+    public class TipoLicenciasController : Controller
+    {
+
+        private static string _baseurl;
+
+        public TipoLicenciasController()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            _baseurl = builder.GetSection("ApiSettings:BaseUrl").Value;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            List<VWTiposLicenciasViewModel> listado = new List<VWTiposLicenciasViewModel>();
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(_baseurl + "api/TipoLicencia/Listado");
+
+                // Analizar la respuesta en una lista de objetos de modelo
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    jsonResponse = jsonResponse.Replace("[", "");
+                    jsonResponse = jsonResponse.Replace("]", "");
+
+                    JObject jsonObj = JObject.Parse(jsonResponse);
+                    JObject dataObj = (JObject)jsonObj["data"];
+                    string message = (string)jsonObj["message"];
+
+                    ViewBag.Mensaje = message;
+
+                    listado = JsonConvert.DeserializeObject<List<VWTiposLicenciasViewModel>>("[" + dataObj.ToString() + "]");
+                }
+                return View(listado);
+            }
+            //List<ClientesModel> listado = await _serviceApi.List();
+            //return View(listado);
+        }
+    }
+}
---
+++ SistemaLicencias.WebUI/SistemaLicencias.WebUI/Models/VWTiposLicenciasViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaLicencias.WebUI.Models
+{
+    public class VWTiposLicenciasViewModel
+    {
+        public int tili_Id { get; set; }
+        public string tili_Descripcion { get; set; }
+        public int tili_UsuCreacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public DateTime? tili_FechaCreacion { get; set; }
+        public int? tili_UsuModificacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+        public DateTime? tili_FechaModificacion { get; set; }
+        public bool? tili_Estado { get; set; }
+
+    }
+}
